42289c19069d7df937a8316088427f8606004395
marcelaksouza <ma.karoline@hotmail.com>
2024-01-16 21:10:42 +0000

    adding routes and file structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Start the project
+npm install
+npm run dev
--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const http = require('http');
+const getReq = require("./methods/get-request");
+const postReq = require("./methods/post-request");
+const putReq = require("./methods/put-request");
+const deleteReq = require("./methods/delete-request");
+let users = require("./data/users.json");
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((request,response) => {
-    console.log("New request");
+const server = http.createServer((req,res) => {
+    let path = req.url;
+    req.user = user;
+    switch(req.method){
+        case "GET":
+            getReq(req,res);
+            break;
+        case "POST":
+            postReq(req,res);
+            break;
+        case "PUT":
+            putReq(req,res);
+            break;
+        case "DELETE":
+            deleteReq(req,res);
+            break;
+        default:
+            res.statusCode = 404;
+            res.setHeader("content-type","application/json");
+            res.write(
+                JSON.stringify({ user: "Not found",message: "Route not found" })
+            );
+    }
+    res.end();
 });
 
-server.listen(3000,'127.0.0.1',()=>{
-    console.log('Application running on port',3000);
+server.listen(port,'127.0.0.1',()=>{
+    console.log(`Application running on port ${port}`);
 });
